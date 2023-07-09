11b4a42340d63efd5fb75aee98cfadadb2fb17ae
F. Ignacio Vega <128429047+ignaciovega93@users.noreply.github.com>
2023-07-09 01:03:35 -0400

    Create sp_suppliers.sql

---
+++ parte-3/project/retail-data-warehouse/etl/sp_suppliers.sql
@@ -0,0 +1,22 @@
+-- agregamos una restriccion para que no hayan duplicados
+ALTER TABLE dim.suppliers
+ADD CONSTRAINT uniqueness_product_id UNIQUE (product_id);
+
+-- cretamos el ETL
+CREATE OR REPLACE PROCEDURE etl.sp_suppliers()
+LANGUAGE SQL as $$
+	INSERT INTO dim.suppliers (
+		product_id, 
+		name, 
+		is_primary
+	)
+	select 
+		codigo_producto, 
+		nombre, 
+		is_primary
+	from stg.suppliers
+	where is_primary = true
+	ON CONFLICT (product_id) DO NOTHING;
+$$;
+
+call etl.sp_suppliers();
